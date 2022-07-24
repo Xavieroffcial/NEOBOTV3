eea9a703a4c0f1bb6a6c7be7b6b87b3ae816e376
Xavieroffcial <108806813+Xavieroffcial@users.noreply.github.com>
2022-07-24 16:43:54 +0800

    Update help.js

--- message/help.js
+++ message/help.js
@@ -1,18 +1,17 @@
 exports.txtSewabot = (prefix) => {
 return `
- 1-Minggu : Rp5.000
- 2-Minggu : Rp.10.000
- 1-Bulan : Rp.20.000
- 2-Bulan : Rp.30.000
- 3-Bulan : Rp40.000
+ 1-Minggu : Rp.5.000
+ 1-Bulan : Rp.8.000
+ 2-Bulan : Rp.10.000
+ Permanen : Rp.13.000
 `
 }
 exports.txtDonate = (prefix) => {
 return `
 ▷_*DONASI BOT*_
- _- DANA : 08xxxxxxxx_
- _- OVO : 08xxxxxxxx_
- _- GOPAY : 08xxxxxxxx_
+ _- DANA : 085962734007_
+ _- OVO : 083862006229_
+ _- GOPAY : 083862006229_
  _- QRIS : https://bit.ly/3yy0qoZ_
 `
 }
